817ceb6385cf32ae32cdaca02fe6ad800e8deaf8
Julia Alvarenga <112830354+juliasalvarenga@users.noreply.github.com>
2026-03-01 11:27:56 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+window.OUR_WORLD_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA8YYBvkRHxvWMix80ounB5PxGv7CKHwHs",
+  authDomain: "corner-654bf.firebaseapp.com",
+  projectId: "corner-654bf",
+  storageBucket: "corner-654bf.firebasestorage.app",
+  messagingSenderId: "636737685368",
+  appId: "1:636737685368:web:cd33757e5db8f4ed456a8d"
+};
